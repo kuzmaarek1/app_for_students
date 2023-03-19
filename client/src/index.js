e90e533dce63e49001d18f28c870d8b2ae1094af
arm650 <arm650>
2023-03-19 17:08:44 +0100

    Add prettier

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Button} from "components"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Button } from "components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    < Button />
+    <Button />
   </React.StrictMode>
 );
